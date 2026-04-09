35b85df26341f9c7dcc6199eb45f564c8ebdfeba
HC2084 <hc14307@gmail.com>
2026-04-09 15:35:14 -0500

    Static tfs based on first msg

--- scripts/odom_mocap_rot.py
+++ scripts/odom_mocap_rot.py
@@ -4,8 +4,8 @@ import tf2_ros
 import tf2_geometry_msgs
 import numpy as np
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped, Vector3Stamped
-from tf.transformations import quaternion_matrix, inverse_matrix
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import quaternion_matrix
 
 class GlobalToMocapProvider:
     def __init__(self):
@@ -13,6 +13,7 @@ class GlobalToMocapProvider:
 
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.output_child_frame = rospy.get_param('~output_child_frame', 'kingfisher')
 
         self.sub = rospy.Subscriber('/natnet_ros/kingfisher/odom', Odometry, self.callback)
         self.pub = rospy.Publisher('/natnet_ros/mocap_in_global', Odometry, queue_size=10)
@@ -31,38 +32,75 @@ class GlobalToMocapProvider:
             ps_transformed = tf2_geometry_msgs.do_transform_pose(ps, transform)
 
             # 3. ROTATION LOGIC
-            # R1: Room -> VIO Map
+            # Known facts:
+            # - incoming twist.linear is expressed in "world"
+            # - tf global<-world includes the fixed ~90 deg yaw map alignment
             q_map = [transform.transform.rotation.x, transform.transform.rotation.y, 
                      transform.transform.rotation.z, transform.transform.rotation.w]
-            R_world_to_global = quaternion_matrix(q_map)
+            R_world_to_global = quaternion_matrix(q_map)[:3, :3]
 
-            # R2: VIO Map -> Drone Body
-            q_body = [ps_transformed.pose.orientation.x, 
-                      ps_transformed.pose.orientation.y, 
-                      ps_transformed.pose.orientation.z, 
-                      ps_transformed.pose.orientation.w]
-            R_global_to_body = inverse_matrix(quaternion_matrix(q_body))
+            q_source_in_global = [ps_transformed.pose.orientation.x,
+                                  ps_transformed.pose.orientation.y,
+                                  ps_transformed.pose.orientation.z,
+                                  ps_transformed.pose.orientation.w]
+            R_source_to_global = quaternion_matrix(q_source_in_global)[:3, :3]
+            R_global_to_source = R_source_to_global.T
 
-            # 4. Apply both rotations to the velocity vector
+            # 4. Transform twist world -> global -> source body (same point)
             v_world = np.array([msg.twist.twist.linear.x, 
                                 msg.twist.twist.linear.y, 
-                                msg.twist.twist.linear.z, 0])
-            
-            # This step converts 'Room Velocity' to 'Body Velocity' 
-            # while accounting for the map rotation
-            # v_global = np.dot(R_world_to_global, v_world)
-            v_body = np.dot(R_global_to_body, v_world)
+                                msg.twist.twist.linear.z])
+            w_world = np.array([msg.twist.twist.angular.x,
+                                msg.twist.twist.angular.y,
+                                msg.twist.twist.angular.z])
+            v_global = np.dot(R_world_to_global, v_world)
+            w_global = np.dot(R_world_to_global, w_world)
 
-            # 5. Construct Message
+            v_source = np.dot(R_global_to_source, v_global)
+            w_source = np.dot(R_global_to_source, w_global)
+
+            # 5. Optional source-child -> output-child adjoint transform.
+            # For transform target<-source with (R, p):
+            #   w_t = R w_s
+            #   v_t = R v_s + p x (R w_s)
+            source_child = "world"
+            target_child = self.output_child_frame
+            v_out = v_source
+            w_out = w_source
+
+            if target_child != source_child:
+                try:
+                    t_cs = self.tf_buffer.lookup_transform(
+                        target_child, source_child, rospy.Time(0), rospy.Duration(0.05))
+                    q_cs = [t_cs.transform.rotation.x,
+                            t_cs.transform.rotation.y,
+                            t_cs.transform.rotation.z,
+                            t_cs.transform.rotation.w]
+                    R_cs = quaternion_matrix(q_cs)[:3, :3]
+                    p_cs = np.array([t_cs.transform.translation.x,
+                                     t_cs.transform.translation.y,
+                                     t_cs.transform.translation.z])
+                    w_out = np.dot(R_cs, w_source)
+                    v_out = np.dot(R_cs, v_source) + np.cross(p_cs, w_out)
+                except Exception as tf_err:
+                    rospy.logwarn_throttle(
+                        5, "Twist child transform %s<- %s unavailable, using source frame twist: %s",
+                        target_child, source_child, str(tf_err))
+                    target_child = source_child
+
+            # 6. Construct Message
             out_msg = Odometry()
             out_msg.header.stamp = msg.header.stamp
             out_msg.header.frame_id = "global"
-            out_msg.child_frame_id = "mocap_body"
+            out_msg.child_frame_id = target_child
             
             out_msg.pose.pose = ps_transformed.pose
-            out_msg.twist.twist.linear.x = v_body[0]
-            out_msg.twist.twist.linear.y = v_body[1]
-            out_msg.twist.twist.linear.z = v_body[2]
+            out_msg.twist.twist.linear.x = v_out[0]
+            out_msg.twist.twist.linear.y = v_out[1]
+            out_msg.twist.twist.linear.z = v_out[2]
+            out_msg.twist.twist.angular.x = w_out[0]
+            out_msg.twist.twist.angular.y = w_out[1]
+            out_msg.twist.twist.angular.z = w_out[2]
 
             self.pub.publish(out_msg)
 
